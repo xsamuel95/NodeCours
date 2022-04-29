88f80695de961c4187435b2bc4c09ca0b049cd4e
Samuel <samuel.louismarie@gmail.com>
2022-04-29 10:22:46 +0200

    correction

--- src/index.js
+++ src/index.js
@@ -1,5 +1,14 @@
-const path = require('path')
-const fs = require ('js')
+const path = require('path');
+const fs = require ('js');
+
+const cheminAbsoluIndexHtml = ;
+const cheminAbsoluLogTxt = ;
+
+console.log(cheminAbsoluIndexHtml);
+console.log(cheminAbsoluLogTxt);
+
+process.exit(1);
+
 
 const contenuIndexhtml = fs.readFileSync('../assets/index.html');
 const contenuLogtxt = String(fs.readFileSync('log.txt'));
