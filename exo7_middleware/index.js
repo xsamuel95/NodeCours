83c16c42170cdf70febfde3212067b3c62cd5348
Samuel <samuel.louismarie@gmail.com>
2022-04-29 16:48:21 +0200

    correction server

--- exo7_middleware/index.js
+++ exo7_middleware/index.js
@@ -1,31 +1,28 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
+// ICI MIDDLEWARE A EXECUTER AVANT DE TRAITER LA REQUETE.
+  let start = performance.now();
   try {
-    // ICI MIDDLEWARE A EXECUTER AVANT DE TRAITER LA REQUETE.
     console.log(req.httpVersion, req.url, req.method);
-    const t0 = performance.now();
     // FIN MIDDLEWARE
-    if (req.url === '/') {
-      res.writeHead(200, { 'content-type': 'text/html' });
+    if (req.url === "/") {
+      res.writeHead(200, {'content-type':'text/html'});
       res.write('<h1>HOMEPAGE</h1>');
-      sendResponse(res, t0);
     } else {
-      res.writeHead(404, { 'content-type': 'text/html' });
+      res.writeHead(404, {'content-type':'text/html'});
       res.write('<h1>404 Not Found</h1>');
-      sendResponse(res, t0);
     }
   } catch (err) {
-    res.writeHead(500, { 'content-type': 'text/html' });
+    res.writeHead(500, {'content-type':'text/html'});
     res.write('<h1>500 Internal Server Error</h1>');
-    sendResponse(res, t0);
   }
-});
-
-function sendResponse(res, start) {
-  const t1 = performance.now();
-  console.log('la requete a pris : ', t1 - start), ' milliseconds';
-  res.end();
+  // ICI MIDDLEWARE A EXECUTER AVANT D'ENVOYER LA REPONSE
+  //
+  res.end()
+  // ICI MIDDLEWARE A EXECUTER APRES AVOIR ENVOYER LA REPONSE
+  let end = performance.now();
+  console.log("requête a pris", end - start);
 }
 
 server.listen(3000);
